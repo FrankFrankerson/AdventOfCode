8087b5655b3a470bde44335e7571428407b49843
Cas Sitvast <cassitvast@gmail.com>
2024-12-01 15:40:26 +0100

    Cleanup

---
+++ main.py
@@ -0,0 +1,11 @@
+def loadExample():
+    with open('ex_input.txt', 'r') as f:
+        return f.readlines()
+
+def loadInput(ind=0):
+    with open('input.txt', 'r') as f:
+        lines = f.readlines()
+        if ind:
+            return lines[:ind]
+        else:
+            return lines
